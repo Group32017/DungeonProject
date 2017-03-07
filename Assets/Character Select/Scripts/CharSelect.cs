431b75d72a64e9314d66ca2d58c94ea67c7acbfb
Dany <dani.extreame@gmail.com>
2017-03-07 11:35:18 +0000

    Added menu and char select

---
+++ Assets/Character Select/Scripts/CharSelect.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CharSelect : MonoBehaviour {
+
+    public GameObject spotLight;
+    public GameObject spotLight2;
+    private bool loading = false;
+    public Text loadtext;
+ 
+    // Use this for initialization
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (loading == true)
+        {
+            loadtext.color = new Color(loadtext.color.r,loadtext.color.g, loadtext.color.b, Mathf.PingPong(Time.time, 1));
+        }
+    }
+
+    void OnMouseOver()
+    {
+       
+        spotLight.SetActive(true);
+        spotLight2.SetActive(true);
+      
+
+    }
+
+    void OnMouseDown()
+    {
+        if (loading == false)
+        {
+            loading = true;
+            loadtext.text = "Loading...";
+            StartCoroutine(LoadSceneMode());
+
+        }
+    }
+
+    void OnMouseExit()
+    {
+        spotLight.SetActive(false);
+        spotLight2.SetActive(false);
+
+    }
+
+    IEnumerator LoadSceneMode()
+    {
+        yield return new WaitForSeconds(1);
+
+        AsyncOperation async = SceneManager.LoadSceneAsync("Maze1Scene");
+
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+    }
+}
+
---
+++ Assets/MainMenu/Scripts/MenuButtons.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+
+    public GameObject mainMenu;
+    public GameObject aboutMenu;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("CharacterSelectScene");
+    }
+
+    public void About()
+    {
+        aboutMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        aboutMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+}
