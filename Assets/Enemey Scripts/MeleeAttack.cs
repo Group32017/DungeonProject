dcd49c59302666f763aefff37f9b39369b74e4e6
Dany <dani.extreame@gmail.com>
2017-03-09 10:21:44 +0000

    Added melee attack

---
+++ Assets/Enemey Scripts/MeleeAttack.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeAttack : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("hit");
+        IDmg dmg = col.gameObject.GetComponent<IDmg>();
+        if (dmg != null)
+        {
+            dmg.TakeDamage(10);
+        }
+    }
+}
--- Assets/Enemey Scripts/enem.cs
+++ Assets/Enemey Scripts/enem.cs
@@ -3,32 +3,32 @@
 
 public class enem : MonoBehaviour
 {
-    public Transform player;
-    public PlayerHealth _playerHealth;
+    public GameObject player;
     private int timeNo = 0; 
     // Use this for initialization
     private void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     private void Update()
     {
-        var direction = player.position - transform.position;
+        var direction = player.transform.position - transform.position;
         var angle = Vector3.Angle(direction, transform.forward);
-        if (Vector3.Distance(player.position, transform.position) < 15 && angle < 90)
+        if (Vector3.Distance(player.transform.position, transform.position) < 10 && angle < 90)
         {
             direction.y = 0;
 
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 0.1f);
 
-            if (direction.magnitude < 8 && direction.magnitude > 2)
+            if (direction.magnitude < 6 && direction.magnitude > 2)
                 transform.Translate(0, 0, 0.05f);
             else if (direction.magnitude < 2)
             {
                 if (timeNo == 40)
                 {
-                   // _playerHealth.ApplyDamage(5);
+                    GlobalValues.PlayerHealth -= 5;
                     timeNo = 0;
                 }
                 else
